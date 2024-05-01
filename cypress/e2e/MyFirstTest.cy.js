8426f1fbf50ff248ace7b01de12bea23c9e42d65
Moynul islam <mail2moynul@gmail.com>
2024-05-01 02:55:53 -0700

    Test case added

--- cypress/e2e/MyFirstTest.cy.js
+++ cypress/e2e/MyFirstTest.cy.js
@@ -2,8 +2,13 @@
 //import { describe } from "mocha";
 
 describe('Test Suite',()=>{
-    it('test title name',()=>{
+    it('test title name positive',()=>{
         cy.visit("https://www.google.com")
         cy.title().should('eq','Google')      
+    })
+
+    it('test title name negative',()=>{
+        cy.visit("https://www.google.com")
+        cy.title().should('eq','Google1')      
     })
 })
